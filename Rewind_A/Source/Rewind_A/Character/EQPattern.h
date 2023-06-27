aacefcd317aa8e8162ffbe634c9376ff4100fe5d
lhy0011 <hamgwa@naver.com>
2023-06-27 17:59:49 +0900

    (8)

---
+++ Rewind_A/Source/Rewind_A/Character/EQPattern.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraShakeBase.h"
+#include "EQPattern.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REWIND_A_API UEQPattern : public UCameraShakePattern
+{
+	GENERATED_BODY()
+	
+
+};
---
+++ Rewind_A/Source/Rewind_A/Character/EQShake.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraShakeBase.h"
+#include "EQShake.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class REWIND_A_API UEQShake : public UCameraShakeBase
+{
+	GENERATED_BODY()
+	
+};
+
